415451fcceaa09820fc674ff2e18de49c7ca1e53
downhillsoft <downhillsoft@gmail.com>
2017-11-18 11:53:24 +0900

    20171118-4

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
 self.addEventListener('install',function(event){
-	
+	event.waitUntil(
+			caches.open('mycache1')
+			.then(function(cache){
+			console.log('sw.js has opened cache..');
+			return cache.addAll('/cached.css');
+			})
+		);	
 });
