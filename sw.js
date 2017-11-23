3f45955fe81f2b3cdaf76d10b65c35b5b221bb81
downhillsoft <downhillsoft@gmail.com>
2017-11-23 22:10:18 +0900

    20171123-5

--- sw.js
+++ sw.js
@@ -4,8 +4,22 @@ self.addEventListener('install',function(event){
 	event.waitUntil(
 			caches.open('dsblackstrawberrycache_v201711231')
 			.then(function(cache){
-			console.log('sw.js has opened cache..');
+			console.log('sw.js will cache files');
 			return cache.addAll(urlsToCache);
 			})
 		);	
+});
+
+self.addEventListener('fetch',function(event){
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response){
+				if(response){
+				 console.log('responding from cache');
+				 return response;
+				}
+				return fetch(event.request);
+			  }
+			)
+	);
 });
