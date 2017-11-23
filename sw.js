07c44f0886d2acac4459bcfdf4d2ae6798d81533
downhillsoft <downhillsoft@gmail.com>
2017-11-23 22:28:09 +0900

    20171123-6

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
-var urlsToCache = ['/bstimer.html','/memo.txt'];
+
+var cacheName = 'dsblackstrawberrycache_v201711231-2';
+
+var urlsToCache = [
+'/bstimer.html'
+,'/memo.txt'
+,'/img/blackstrawberry.png'
+,'del.png'
+];
 
 self.addEventListener('install',function(event){
 	event.waitUntil(
-			caches.open('dsblackstrawberrycache_v201711231')
+			caches.open(cacheName)
 			.then(function(cache){
 			console.log('sw.js will cache files');
 			return cache.addAll(urlsToCache);
 			})
 		);	
 });
+
+
+self.addEventListener('activate', function(e) {
+  console.log('handling activate event..');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
 
 self.addEventListener('fetch',function(event){
 	event.respondWith(
